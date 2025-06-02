a16ac1b0d32a8d9a483dc0bf969720dba4c2e76c
almasdii <164005653+almasdii@users.noreply.github.com>
2025-06-02 22:46:27 +0500

    Update ClassicalMusic.java

--- Lesson11.SpringAnnotations3/src/main/java/ru/alishev/springcourse/ClassicalMusic.java
+++ Lesson11.SpringAnnotations3/src/main/java/ru/alishev/springcourse/ClassicalMusic.java
@@ -3,7 +3,7 @@ package ru.alishev.springcourse;
 import org.springframework.stereotype.Component;
 
 /**
- * @author Neil Alishev
+ * 
  */
 @Component
 public class ClassicalMusic implements Music {
